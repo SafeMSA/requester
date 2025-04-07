07f4bb391d44f4d061015d7b20b5895968ed7839
martinaskolin <martinaskolin99@gmail.com>
2025-04-07 12:21:04 +0200

    ajsjs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor
 import time
-import random
+from random import randrange
 import requests
 
 # Function to send GET request
@@ -12,10 +12,10 @@ def send_get_request(url):
         print(f"Error: {e}")
 
 while True:
-    time.sleep(random.random(10, 30))
+    time.sleep(randrange(10, 30))
 
     # List of URLs to send GET requests to
-    urls = ['http://localhost:9092'] * random.random(1, 10)
+    urls = ['http://localhost:9092'] * randrange(1, 10)
 
     # Use ThreadPoolExecutor to send requests in parallel
     with ThreadPoolExecutor(max_workers=5) as executor:
